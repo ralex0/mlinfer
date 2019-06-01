06c7415a6b3b12e1209ac8263c99f26b3e6f0e85
Ron Alexander <ralexander@fas.harvard.edu>
2019-06-01 14:06:13 -0400

    Change package name in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
-from holo import __version__
+from mlinfer import __version__
 
-setup(name='holo',
+setup(name='mlinfer',
+      packages=['mlinfer'],
       version=__version__,
-      packages=['holo'],
-      entry_points={'console_scripts': ['holo = holo.__main__:main']})
+      entry_points={'console_scripts': ['mlinfer = mlinfer.__main__:main']})
